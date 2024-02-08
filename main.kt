faa6a7bae96942100ae92da259a9c3fa11f1cc69
DeveloperZone <150701476+DevZonePage@users.noreply.github.com>
2024-02-08 14:16:42 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,3 +1,15 @@
-fun main(args: Array<String>) {
-  println("Hello world!")
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    // Read an integer from the user
+    val n: Int = sc.nextInt()
+
+    // Display the input back to the user
+    println("You entered: $n")
+
+    // Close the Scanner to avoid resource leaks
+    sc.close()
 }
